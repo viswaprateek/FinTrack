19e2a609074e377b2fad7f3a5df54121c204f8ec
viswa <viswaprateek248@gmail.com>
2026-06-08 22:56:44 +0530

    cors error fixed

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -22,6 +22,8 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "https://fin-track-l9qe.vercel.app",
+
     ]
 
 
